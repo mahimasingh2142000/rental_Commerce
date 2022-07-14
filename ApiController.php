7b29e2f9c43323df021b054520933f520b1f93d1
mahimasingh2142000 <103221697+mahimasingh2142000@users.noreply.github.com>
2022-07-14 10:33:28 -0700

    Add files via upload

---
+++ ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function apilist()
+    {
+        return "this is from api";
+    }
+
+    //
+}
